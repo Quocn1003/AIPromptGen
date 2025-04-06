82e88733b34bf534dba19274f4999e5fde825616
quocn1003 <quocn1003@gmail.com>
2025-04-07 05:47:53 +0700

    Fix for local

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // vite.config.js for github pages
-export default defineConfig({
-  base: '/AIPromptGen/', // Thay bằng tên repo trên GitHub
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    base: command === 'build' ? '/AIPromptGen/' : '/',
+  }
 })
 
